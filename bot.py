143352fcc4eb1b18c7f4c091feb5a204676dd967
Lucas <lucaskumara@hotmail.com>
2021-03-24 20:31:50 -0400

    Created core of bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import logging
+import os
+
+from discord.ext import commands
+from configparser import ConfigParser
+
+# Load config
+config = ConfigParser()
+config.read('config.ini')
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s: %(name)s] %(levelname)s - %(message)s'
+)
+
+# Intantiate bot
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('+'))
+bot.logger = logging.getLogger('bot')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+if __name__ == '__main__':
+    bot.run(config['BOT']['TOKEN'])
---
+++ cogs/admin.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    '''Admin only commands to manipulate the bot.'''
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.delete_delay = 5
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def load(self, ctx, extension):
+        '''Loads an extension.'''
+        try:
+            self.bot.load_extension(f'cogs.{extension}')
+        except commands.ExtensionError as error:
+            await ctx.message.add_reaction('👎')
+            self.bot.logger.info(f'{error.__class__.__name__}: {error}')
+        else:
+            await ctx.message.add_reaction('👍')
+        finally:
+            await ctx.message.delete(delay=self.delete_delay)
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def unload(self, ctx, extension):
+        '''Unloads an extension.'''
+        try:
+            self.bot.unload_extension(f'cogs.{extension}')
+        except commands.ExtensionError as error:
+            await ctx.message.add_reaction('👎')
+            self.bot.logger.info(f'{error.__class__.__name__}: {error}')
+        else:
+            await ctx.message.add_reaction('👍')
+        finally:
+            await ctx.message.delete(delay=self.delete_delay)
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def reload(self, ctx, extension):
+        '''Reloads an extension.'''
+        try:
+            self.bot.reload_extension(f'cogs.{extension}')
+        except commands.ExtensionError as error:
+            await ctx.message.add_reaction('👎')
+            self.bot.logger.info(f'{error.__class__.__name__}: {error}')
+        else:
+            await ctx.message.add_reaction('👍')
+        finally:
+            await ctx.message.delete(delay=self.delete_delay)
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
