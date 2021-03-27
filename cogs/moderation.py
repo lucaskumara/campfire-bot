d6339e386994174be15a2f71574ab9a41bf25d30
Lucas <lucaskumara@hotmail.com>
2021-03-27 01:32:04 -0400

    Created unban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -19,5 +19,19 @@ class Moderation(commands.Cog):
         await ctx.guild.ban(member, reason=reason)
         await ctx.send(f'{member} has been banned.')
 
+    @commands.command()
+    async def unban(self, ctx, member, *, reason=None):
+        '''Unbans a member from the server.'''
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'{user} has been unbanned.')
+                return
+
 def setup(bot):
     bot.add_cog(Moderation(bot))
