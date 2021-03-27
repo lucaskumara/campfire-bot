305ac8e5dc00ffb11a0efd8311baf08addce4de8
Lucas <lucaskumara@hotmail.com>
2021-03-27 00:16:39 -0400

    Created kick command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,5 +7,11 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        '''Kicks a member from the server.'''
+        await ctx.guild.kick(member, reason=reason)
+        await ctx.send(f'{member} has been kicked.')
+
 def setup(bot):
     bot.add_cog(Moderation(bot))
