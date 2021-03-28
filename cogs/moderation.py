2aac44de5ecd3ff777b95ffe2b2be5e5e24bc873
Lucas <lucaskumara@hotmail.com>
2021-03-28 15:06:21 -0400

    Added optional tempban functionality to ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,8 @@
 import discord
+import asyncio
 
 from discord.ext import commands
+from typing import Optional
 
 
 class BannedUser(commands.Converter):
@@ -21,6 +23,19 @@ class BannedUser(commands.Converter):
 
         raise commands.BadArgument(message='Banned user not found')
 
+class TimePeriod(commands.Converter):
+    '''Converter for checking time intervals.'''
+
+    async def convert(self, ctx, argument):
+        '''Converts argument to a tuple representing a period of time.'''
+        amount = argument[:-1]
+        unit = argument[-1]
+
+        if amount.isdigit() and unit in ['s', 'm', 'h', 'd', 'w', 'm', 'y']:
+            return (int(amount), unit)
+
+        raise commands.BadArgument(message='Not a valid period of time')
+
 
 class Moderation(commands.Cog):
     '''Cog containing commands for server moderation.'''
@@ -35,10 +50,33 @@ class Moderation(commands.Cog):
         await ctx.send(f'{member} has been kicked.')
 
     @commands.command()
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
+    async def ban(self, ctx, member: discord.Member, duration: Optional[TimePeriod] = None, *, reason=None):
         '''Bans a member from the server.'''
         await ctx.guild.ban(member, reason=reason)
-        await ctx.send(f'{member} has been banned.')
+
+        if duration is None:
+            await ctx.send(f'{member} has been banned.')
+        else:
+            await ctx.send(f'{member} has been temporarily banned.')
+            
+            multiplier = {
+                's': 1,
+                'm': 60,
+                'h': 3600,
+                'd': 86400,
+                'w': 604800,
+                'm': 2592000,
+                'y': 31536000
+            }
+
+            amount, unit = duration
+            await asyncio.sleep(amount * multiplier[unit])
+
+            # Check if user is still banned
+            ban_entry = discord.utils.find(lambda e: e.user == member, await ctx.guild.bans())
+
+            if ban_entry is not None:
+                await ctx.guild.unban(member, reason='Tempban expired')       
 
     @commands.command()
     async def unban(self, ctx, user: BannedUser, *, reason=None):
