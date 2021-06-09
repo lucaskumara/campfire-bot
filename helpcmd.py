95f92047e299f6a27b8964f0a6a86799aa11a2fd
Lucas <lucaskumara@hotmail.com>
2021-06-09 15:20:40 -0400

    Added help command handling group arguments + cleaned up code

--- helpcmd.py
+++ helpcmd.py
@@ -26,6 +26,7 @@ class HelpCommand(commands.HelpCommand):
 
         # Gets the valid command prefixes
         command_prefixes = await self.context.bot.get_prefix(self.context.message)
+        command_prefix = command_prefixes[-1]
         
         # Create help embed
         help_embed = discord.Embed(
@@ -40,7 +41,8 @@ class HelpCommand(commands.HelpCommand):
         # Create a field for each command except admin commands
         for cog in mapping:
             if cog is not None and cog.qualified_name != 'Admin' and cog.get_commands() != []:
-                help_embed.add_field(name=cog.qualified_name, value='```\n' + f'help {cog.qualified_name}' + '```', inline=False)
+                field_value = f'```\n{command_prefix}help {cog.qualified_name}```'
+                help_embed.add_field(name=cog.qualified_name, value=field_value)
 
         await self.context.reply(embed=help_embed)
 
@@ -52,6 +54,10 @@ class HelpCommand(commands.HelpCommand):
             await self.send_error_message(f'No command called "{cog.qualified_name}" found.')
             return
 
+        # Get command names
+        command_names = [command.name for command in cog.get_commands()]
+        commands_string = '```\n' + '\n'.join(command_names) + '```'
+
         # Create cog embed
         cog_embed = discord.Embed(
             title=cog.qualified_name,
@@ -61,13 +67,44 @@ class HelpCommand(commands.HelpCommand):
         )
 
         cog_embed.set_author(name='Campfire', icon_url=self.context.bot.user.avatar_url)
+        cog_embed.add_field(name='Commands', value=commands_string, inline=False)
         cog_embed.set_footer(text=f'Requested by {self.context.author}', icon_url=self.context.author.avatar_url)
 
-        # Get commands and show usages
-        command_names = [command.name for command in cog.get_commands()]
-        cog_embed.add_field(name='Commands', value=f'```\n' + '\n'.join(command_names) + '```', inline=False)
-
         await self.context.reply(embed=cog_embed)
+
+    async def send_group_help(self, group):
+        '''Called when the help command is called with a group argument.'''
+
+        # Don't show admin commands
+        if group.cog.qualified_name == 'Admin':
+            await self.send_error_message(f'No command called "{group.name}" found.')
+            return
+
+        # Gets the valid command prefixes
+        command_prefixes = await self.context.bot.get_prefix(self.context.message)
+        command_prefix = command_prefixes[-1]
+
+        # Create usage string
+        usage_string = f'```\n{command_prefix}{group.usage}```'
+
+        # Get subcommand names
+        subcommand_names = [f'{group.name} {command.name}' for i, command in enumerate(group.commands)]
+        subcommands_string = '```\n' + '\n'.join(subcommand_names) + '```'
+
+        # Create group embed
+        group_embed = discord.Embed(
+            title=group.name.capitalize(),
+            description=group.help,
+            colour=discord.Colour.orange(),
+            timestamp=self.context.message.created_at
+        )
+
+        group_embed.set_author(name='Campfire', icon_url=self.context.bot.user.avatar_url)
+        group_embed.add_field(name='Usage', value=usage_string, inline=False)
+        group_embed.add_field(name='Subcommands', value=subcommands_string, inline=False)
+        group_embed.set_footer(text=f'Requested by {self.context.author}', icon_url=self.context.author.avatar_url)
+
+        await self.context.reply(embed=group_embed)
 
     async def send_command_help(self, command):
         '''Called when the help command is called with a command argument.'''
@@ -79,6 +116,7 @@ class HelpCommand(commands.HelpCommand):
 
         # Gets the valid command prefixes
         command_prefixes = await self.context.bot.get_prefix(self.context.message)
+        command_prefix = command_prefixes[-1]
 
         # Create command embed
         command_embed = discord.Embed(
@@ -89,7 +127,7 @@ class HelpCommand(commands.HelpCommand):
         )
 
         command_embed.set_author(name='Campfire', icon_url=self.context.bot.user.avatar_url)
-        command_embed.add_field(name='Usage', value=f'```\n{command_prefixes[-1]}{command.usage}```', inline=False)
+        command_embed.add_field(name='Usage', value=f'```\n{command_prefix}{command.usage}```', inline=False)
         command_embed.set_footer(text=f'Requested by {self.context.author}', icon_url=self.context.author.avatar_url)
 
         await self.context.reply(embed=command_embed)
