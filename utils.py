488c788fb23389f698e72599b3312394e280a4f4
Lucas <lucaskumara@hotmail.com>
2022-06-29 16:43:38 -0400

    Created evaluate_exception to properly identify errors

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import hikari
+import lightbulb
+import typing
+
 from datetime import datetime, timezone
 
 INFO_EMBED_COLOUR = hikari.Colour(0xE67E22)
@@ -80,3 +83,30 @@ async def clone_channel(
         category=kwargs.get("category", channel.parent_id),
     )
     return clone_channel
+
+
+def evaluate_exception(
+    exception: lightbulb.LightbulbError,
+    exception_type: typing.Type[lightbulb.LightbulbError],
+):
+    """Evaluates whether an exception is of or contains a specified type.
+
+    Check if the exception type is the specified type. If it isn't, check its causes to
+    see if it contains the exception type.
+
+    Arguments:
+        exception: The exception to check.
+        exception_type: The type to check for.
+
+    Returns:
+        True if the except is or contains the type, false if not.
+    """
+    if type(exception) is exception_type:
+        return True
+
+    if hasattr(exception, "causes"):
+        for cause in exception.causes:
+            if type(cause) is exception_type:
+                return True
+
+    return False
