4e5e4bffe283cf539dd8f5cf94ddf0f20afed933
Lucas <lucaskumara@hotmail.com>
2022-07-11 20:28:51 -0400

    Corrected bot prefixes

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import hikari
 import lightbulb
 import configparser
 
+
 config = configparser.ConfigParser()
 config.read("config.ini")
+
 
 if __name__ == "__main__":
     bot = lightbulb.BotApp(
         token=config.get("BOT", "TOKEN"),
-        prefix=lightbulb.when_mentioned_or(["campfire", "camp"]),
+        prefix=lightbulb.when_mentioned_or(["campfire ", "camp "]),
     )
 
     bot.load_extensions_from("./extensions")
