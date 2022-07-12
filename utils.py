e70d7f7e19da62a7e618828163dac4b35ec7a024
Lucas <lucaskumara@hotmail.com>
2022-07-11 20:29:10 -0400

    Implemented info_response and error_response

--- utils.py
+++ utils.py
@@ -7,10 +7,10 @@ from datetime import datetime, timezone
 
 INFO_EMBED_COLOUR = hikari.Colour(0xE67E22)
 ERROR_EMBED_COLOUR = None  # hikari.Colour(0xE74C3C)
-DELETE_ERROR_DELAY = 10
+ERROR_DELETE_DELAY = 10
 
 
-def create_info_embed(title: str, description: str, icon: str) -> hikari.Embed:
+def create_info_embed(title: str, description: str, icon: hikari.URL) -> hikari.Embed:
     """Creates and returns an embed to display information to users.
 
     Creates an instance of an embed and fills it with the data provided by the
@@ -34,6 +34,25 @@ def create_info_embed(title: str, description: str, icon: str) -> hikari.Embed:
     return embed
 
 
+async def info_response(
+    context: lightbulb.Context, title: str, description: str
+) -> None:
+    """Creates an info response and sends it via the passed in context.
+
+    Arguments:
+        context: The context of the command.
+        title: The title of the response embed.
+        description: The description of the response embed.
+
+    Returns:
+        None.
+    """
+    bot_avatar_url = context.app.get_me().avatar_url
+    info_embed = create_info_embed(title, description, bot_avatar_url)
+
+    await context.respond(embed=info_embed)
+
+
 def create_error_embed(description: str, icon: str) -> hikari.Embed:
     """Creates and returns an embed to display errors to users.
 
@@ -54,6 +73,22 @@ def create_error_embed(description: str, icon: str) -> hikari.Embed:
     )
     embed.set_author(name="Campfire", icon=icon)
     return embed
+
+
+async def error_response(context: lightbulb.Context, description: str) -> None:
+    """Creates an error response and sends it via the passed in context.
+
+    Arguments:
+        context: The context of the command.
+        description: The description of the response embed.
+
+    Returns:
+        None.
+    """
+    bot_avatar_url = context.app.get_me().avatar_url
+    error_embed = create_error_embed(description, bot_avatar_url)
+
+    await context.respond(embed=error_embed, delete_after=ERROR_DELETE_DELAY)
 
 
 async def clone_channel(
@@ -111,15 +146,3 @@ def evaluate_exception(
                 return True
 
     return False
-
-
-def get_bot_avatar_url(plugin: lightbulb.Plugin) -> hikari.URL:
-    """Gets the bot avatar URL from a plugin.
-
-    Arguments:
-        plugin: The plugin to get the URL from.
-
-    Returns:
-        The URL of the bots avatar.
-    """
-    return plugin.bot.get_me().avatar_url
